475f5c3b73868476bf55897ca9ad4a662d8f3a4b
NeelRaval-hacker <pnraval2007@gmail.com>
2020-06-14 17:02:16 +0530

    Headers and Components

--- src/pages/Tabs/News.js
+++ src/pages/Tabs/News.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import { IonPage, IonContent } from '@ionic/react';
+import SmallHeader from '../../components/Header/SmallHeader';
+import LargeHeader from '../../components/Header/LargeHeader';
 
 const News = ( ) => { 
     return ( 
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>
-                        News
-                    </IonTitle>
-                </IonToolbar>
-            </IonHeader>
+            <SmallHeader title = "News" />
+            <IonContent fullscreen>
+                <LargeHeader title = "News" /> 
+            </IonContent>
         </IonPage>
     );
 };
