399b1397b765100b70ba2dcd8f3bcf61f414c08b
NeelRaval-hacker <pnraval2007@gmail.com>
2020-06-14 17:22:28 +0530

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAa8w0-cbCLcvHaABEjYAQ1xjMG6uoKNW0",
+    authDomain: "news-9553f.firebaseapp.com",
+    databaseURL: "https://news-9553f.firebaseio.com",
+    projectId: "news-9553f",
+    storageBucket: "news-9553f.appspot.com",
+    messagingSenderId: "73175888657",
+    appId: "1:73175888657:web:b8c8ed570888f1fe2092b0",
+    measurementId: "G-NJP6NLT3LX"
+  };
+
+  export default firebaseConfig;
